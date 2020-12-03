956a1937fe0e451e8806b69208e2a7d4461ac73d
YmkeVandebroek <51299350+YmkeVandebroek@users.noreply.github.com>
2020-12-03 13:31:24 +0100

    3/12/20 CSS

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-/*const _ = require("lodash"); */
-
-
 var vm = new Vue({
 	el: '#app',
-	data: function () {
+	data () {
 		return {
 			search: null,
-			video_id: null/*'zKmsZzJQJV0'*/,
+			video_id: null,
 			queue: [],
-			playing: [],
 			searchResults: [],
-			/*video_id: null,*/
+			currentVideo: null,
+			isplaying: false,
 			playerVars: {
 				autoplay: 1,
 				controls: 0,
-				/*rel:1*/
 			}
 		};
 	},
@@ -24,166 +20,72 @@ var vm = new Vue({
 		}
 	},
 	methods: {
-		searchVideos: function() {
+		searchVideos () {
 			var self = this;
-			/*var queue = ;*/
 			var search = encodeURI(this.search);
 			axios.get('https://vuetv.acmoore.co.uk/search/'+search).then(function (response) {
-				var first_result = response.data[0];
-				self.loadVideo(first_result.video_id);
-				console.log(response.data);
-
-        self.searchResults = response.data;
-
-        /*for (var i = 0; i < response.data.length; i++) {
-					response.data[i]
-					console.log(response.data[i].image);
-					console.log(response.data[i].video_id);
-					console.log(response.data[i].title);
-
-				}*/
-				console.log('-----------------efkes iets anders---------')
-				console.log(response.data)
-
+				self.searchResults = response.data;
+				console.log(response);       		
 			});
-			/* --------changes h1 to playing title
-			let videoTitle = document.getElementById("videoTitle");
+		},
+		loadVideo 	(video) {
+			this.player.loadVideoById(video.video_id);
+			this.currentVideo = video;
+			console.log(video);
+			/*let videoTitle = document.getElementById("videoTitle");
 			let videoTitleRequest = (response.data[0].title);
 			videoTitle.innerHTML = videoTitleRequest;*/
 		},
-		loadVideo: function (video_id) {this.player.loadVideoById(video_id);},
-		playVideo: function () {this.player.playVideo();},
-		pauseVideo: function () {this.player.pauseVideo();},
-		stopVideo: function () {this.player.stopVideo()},
-		setVolume: function () {
+		playVideo 	() {this.player.playVideo();},
+		pauseVideo 	() {this.player.pauseVideo();},
+		stopVideo 	() {this.player.stopVideo()},
+		setVolume 	() {
 			let volumeValue = document.getElementById("volume");
 			this.player.setVolume(volumeValue.value)
 		},
-		addQueue:	function (video) {
+		addQueue (video) {
 			this.queue.push(video);
-			console.log(video.video_id);
-
-			for (var i = 0; i < this.queue.length; i++) {
-				console.log(this.queue[i])
-			}
-			/*console.log(queue);*/
-
+			console.log(video);
+			for (var i = 0; i < this.queue.length;i++){console.log(this.queue[i])};
 			this.searchResults = []
+			
+			if (this.isplaying===false) {
+				video = this.queue.shift();
+				this.loadVideo(video);
+			} else {
+				
+			}
 		},
-		removeQueue:	function (video) {
+		removeQueue (video) {
 			console.log("delete");
 			console.log(this.queue);
-
-
-			this.queue = _.reject(this.queue,
+			
+			this.queue = _.reject(this.queue, 
 				{ 'video_id': video.video_id});
-
-			/*_.reject(queue,
-				{ 'video_id': "Nskh_L0IHmc"}); */
-
-
-			/*_.reject(queue,
-					{ 'video_id': this.video_id});	*/
-
-
-			/*_.reject(queue, {
-				for (let i = 0; i < queue.length; i++) {
-					'video_id': queue[i].video_id
-
-				}
-			});*/
-			/*queue.remove();*/
-			/*this.queue.remove(this.video_id);
-			vue.delete(this.video)*/
-			/*<button @click="removeQueue">remove queue</button>*/
+		},
+		ready(){
+			console.log("ready");
+			
+		},
+		skipVideo(){
+			console.log("skipped");
+			video = this.queue.shift();
+			this.loadVideo(video);
+		},
+		playing(){
+			console.log("playing");
+			this.isplaying = true;
+		},
+		ended(){
+			console.log("ended");
+			this.isplaying = false;
+			
 		},
 	}
 });
 
 
-/*### [Search by keyword](/javascript/search.js)
 
-Method: youtube.search.list<br>
-Description: This code sample calls the API's <code>search.list</code> method to retrieve search results associated
-with a particular keyword. The HTML page uses JQuery, along with the <code>auth.js</code> and <code>search.js</code> JavaScript files, to show a simple search form and display the list of search results.*/
-
-/* Auto complete
-$("#youtube").autocomplete({
-    source: function(request, response){
-        // API KEY
-        //var apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
-				var apiKey = 'AIzaSyAnalLPwbNqLXYOBCAKRHW_uyRL_vTYngU' // own api
-        // Search term
-        var query = request.term;
-
-        $.ajax({
-            url: "https://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q="+query+"&key="+apiKey+"&format=5&alt=json&callback=?",
-            dataType: 'jsonp',
-            success: function(data, textStatus, request) {
-               response( $.map( data[1], function(item) {
-                    return {
-                        label: item[0],
-                        value: item[0]
-                    }
-                }));
-            }
-        });
-    },
-    select: function( event, ui ) {
-        $.youtubeAPI(ui.item.label);
-    }
-});
- */
-/*
-// Button
-$('button#submit').click(function(){
-    var value = $('input#youtube').val();
-        $.youtubeAPI(value);
-});
-// YouTube search request
-$.youtubeAPI = function(search){
-    $.ajax({
-        type: 'GET',
-        url: 'https://gdata.youtube.com/feeds/api/videos?q=' + search + '&max-results=5&v=2&alt=jsonc',
-        dataType: 'jsonp',
-        success: function( info ){
-            if( info.data.items ){
-					      result.empty();
-                $.each( info.data.items, function(i, data) {
-                    result.append('<div class="youtube">\
-                        <img src="' + data.thumbnail.sqDefault + '" alt="" />\
-                        <h3><a href="javascript:void(0)" onclick="$.youtubePlay(\'' + data.id + '\', \'' + data.content[5] + '\')">' + data.title + '</a></h3>\
-                        <p>' + data.description + '</p>\
-                    </div>\
-                    <div class="youtubeOynat" id="' + data.id + '"></div>');
-                });
-            }
-            else {
-                result.html('<div class="hata">' + search + '</div>');
-            }
-        }
-    });
-}
-*/
-
-/* -------------TEST CODE-----------------
-
-setInterval(function () {
-	console.log("logging");
-	console.log(data.discription[0]);
-	console.log(data);
-	console.log(discription);
-	console.log(info);
-	console.log(search);
-	console.log(info.data.items);
-}, 500);
-
-	getVideoTitle: function () {
-		let videoTitle = document.getElementById("videoTitle");
-		let title = snippet.title;
-		videoTitle.innerHTML= title.innerHTML;
-	},
-	*/
 
 let modeButton = document.getElementById("modebutton");
 modeButton.addEventListener("click", switchMode);
